622002c6dd4c93cb5eba6de1bb475958eea0c747
Sattvik Chakravarthy <sattvik@gmail.com>
2016-02-20 18:59:20 +0530

    Working workflow example with a simple expression.

--- workflow/task/builtin.py
+++ workflow/task/builtin.py
@@ -12,3 +12,31 @@ class DummyTask(Task):
         self.set_output('a', 'AAA')
         self.set_output('b', 'BBB')
         self.set_output('c', 'CCC')
+
+
+class AddTask(Task):
+    ID = 'builtin.add'
+
+    def run(self):
+        a = self.get_input('a')
+        b = self.get_input('b')
+        c = a + b
+        self.set_output('c', c)
+
+
+class MultiplyTask(Task):
+    ID = 'builtin.multiply'
+
+    def run(self):
+        a = self.get_input('a')
+        b = self.get_input('b')
+        c = a * b
+        self.set_output('c', c)
+
+
+class PrintTask(Task):
+    ID = 'builtin.print'
+
+    def run(self):
+        for k, v in self.inputs.items():
+            print(k, '=', v)
